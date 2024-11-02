7d3ec5d6e4db19e6dd9f7811b247c08adcca294f
hichamelhirch <hichamelhirchgmi@gmail.com>
2024-11-02 20:42:10 +0100

    V2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PlayingCardComponent } from "./Components/playing-card/playing-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PlayingCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
